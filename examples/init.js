3827f9539b3600408b88898df9b8fb022c0b4bdf
scotty <scotty@burak.pl>
2009-09-01 06:00:51 +0200

    Corrected misleading comment

--- examples/init.js
+++ examples/init.js
@@ -1,5 +1,5 @@
 /*
- * This code searches for all the <script type="application/processing" target="canvasid">
+ * This code searches for all the <script type="application/processing" src="#canvasid">
  * in your page and loads each script in the target canvas with the proper id.
  * It is useful to smooth the process of adding Processing code in your page and starting
  * the Processing.js engine.
