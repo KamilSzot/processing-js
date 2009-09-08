abd5da63f775c93faf428be298d74970136da685
scotty <scotty@burak.pl>
2009-09-08 03:52:19 +0200

    Made examples/init.js crossbrowser, (working in IE)

--- examples/init.js
+++ examples/init.js
@@ -4,26 +4,33 @@
  * It is useful to smooth the process of adding Processing code in your page and starting
  * the Processing.js engine.
  */
+// Cross-browser implementation of element.addEventListener()
 
-if ( window.addEventListener ) {
-	window.addEventListener("load", function() {
-		var scripts = document.getElementsByTagName("script");
-		
-		for ( var i = 0; i < scripts.length; i++ ) {
-			if ( scripts[i].type == "application/processing" ) {
-				var src = scripts[i].src, canvas = scripts[i].nextSibling;
+(function(evnt, elem, func) {
+    if (elem.addEventListener)  // W3C DOM
+        elem.addEventListener(evnt,func,false);
+    else if (elem.attachEvent) { // IE DOM
+         var r = elem.attachEvent("on"+evnt, func);
+	return r;
+    }
+    else window.alert('I\'m sorry Dave, I\'m afraid I can\'t do that.');
+})("load", window, function() {
+	var scripts = document.getElementsByTagName("script");
 	
-				if ( src && src.indexOf("#") ) {
-					canvas = document.getElementById( src.substr( src.indexOf("#") + 1 ) );
-				} else {
-					while ( canvas && canvas.nodeName.toUpperCase() != "CANVAS" )
-						canvas = canvas.nextSibling;
-				}
+	for ( var i = 0; i < scripts.length; i++ ) {
+		if ( scripts[i].type == "application/processing" ) {
+			var src = scripts[i].src, canvas = scripts[i].nextSibling;
 
-				if ( canvas ) {
-					Processing(canvas, scripts[i].text);
-				}
+			if ( src && src.indexOf("#") ) {
+				canvas = document.getElementById( src.substr( src.indexOf("#") + 1 ) );
+			} else {
+				while ( canvas && canvas.nodeName.toUpperCase() != "CANVAS" )
+					canvas = canvas.nextSibling;
+			}
+
+			if ( canvas ) {
+				Processing(canvas, scripts[i].text);
 			}
 		}
-	}, false);
-}
+	}
+});
